4e223389171fc1dd8996d8e8de3215dc10ed9bf4
Merrouche Aimen Rami <merrouche.aimenrami@esst-sup.com>
2024-11-26 11:51:14 +0100

    Update criteria_tables.blade.php

--- resources/views/wsm/criteria_tables.blade.php
+++ resources/views/wsm/criteria_tables.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
+@include('partials._navbar')
     <div class="container">
         <h2>Criteria and Weights</h2>
         <table class="table">
